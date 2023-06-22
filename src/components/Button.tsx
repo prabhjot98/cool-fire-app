a8b4b6617a6ecc963d4db0731f74c80532698ee7
Michael Burns <mjburns@ucdavis.edu>
2023-06-21 17:02:11 -0700

    wip: useReducer stuff

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,3 +1,41 @@
+import { createContext, useContext, useReducer } from "react";
+
+const NumberContext = createContext<number>(0);
+
+const Number = ({ onDecrement }: { onDecrement: () => void }) => {
+  const number = useContext(NumberContext);
+
+  return (
+    <div>
+      <button type="button" onClick={onDecrement}>
+        Decrement
+      </button>
+      <p>{number}</p>
+    </div>
+  );
+};
+
+type State = { number: number };
+type Action = { type: "Decrement" } | { type: "DivideByTwo" };
+const reducer = (state: State, action: Action) => {
+  if (action.type === "Decrement") {
+    return { number: state.number - 1 };
+  }
+};
+
 export const App = () => {
-  return <div>lol</div>;
+  const [state, dispatch] = useReducer(reducer, { number: 0 } as State);
+
+  return (
+    <NumberContext.Provider value={state.number}>
+      <div className="m-5">
+        <div className="flex">
+          <p>{state.number}</p>
+          <button type="button" onClick={() => dispatch({ type: "Decrement" })}>
+            Decrement!
+          </button>
+        </div>
+      </div>
+    </NumberContext.Provider>
+  );
 };
